3311ecb74cd8aaf58a93da318a2f1172c895c7b6
Bogdan Mihai <bogdan.mosteanu@hey.com>
2026-04-10 21:49:44 +0300

    update tracklist

--- lib/tracklist.ts
+++ lib/tracklist.ts
@@ -4,20 +4,44 @@ export const underNet: Tracklist = [
   {
     title: "crossroad",
     artist: "beansclub",
-    audio_url: "undernet/beansclub-crossroad",
+    audio_url: "undernet-vol-4/beansclub-crossroad",
     permalink_url: "https://soundcloud.com/underdotnet/beansclub-crossroad-1",
   },
   {
     title: "Snake Pit",
     artist: "MEKA",
-    audio_url: "undernet/MEKA-Snake_Pit",
+    audio_url: "undernet-vol-4/MEKA-Snake_Pit",
     permalink_url: "https://soundcloud.com/underdotnet/meka-snake-pit-2",
   },
   {
     title: "Young Birds",
     artist: "✯cenobia✯",
-    audio_url: "undernet/cenobia-Young_Birds",
+    audio_url: "undernet-vol-4/cenobia-Young_Birds",
     permalink_url: "https://soundcloud.com/underdotnet/cenobia-young-birds-3",
+  },
+  {
+    title: "Rea's Song",
+    artist: "Type R",
+    audio_url: "undernet-vol-4/Type-R-Reas_Song",
+    permalink_url: "https://soundcloud.com/underdotnet/type-r-rea_s-song-4",
+  },
+  {
+    title: "immortality",
+    artist: "W07VES",
+    audio_url: "undernet-vol-4/W07VES-immortality",
+    permalink_url: "https://soundcloud.com/underdotnet/w07ves-immortality-5",
+  },
+  {
+    title: "I H3ART YOU",
+    artist: "Hynsk",
+    audio_url: "undernet-vol-4/Hynsk-I_H3ART_YOU",
+    permalink_url: "https://soundcloud.com/underdotnet/hynsk-i-h3art-you-6",
+  },
+  {
+    title: "wisp",
+    artist: "luuxu",
+    audio_url: "undernet-vol-4/luuxu-wisp",
+    permalink_url: "https://soundcloud.com/underdotnet/luuxu-wisp-7",
   },
 ];
 
