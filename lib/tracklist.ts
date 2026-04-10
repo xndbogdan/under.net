82bfb96ab2780434176e135333c9ce0bba9b8f71
Bogdan Mihai <bogdan.mosteanu@hey.com>
2026-04-10 22:07:20 +0300

    full vol 4 tracklist

--- lib/tracklist.ts
+++ lib/tracklist.ts
@@ -22,7 +22,7 @@ export const underNet: Tracklist = [
   {
     title: "Rea's Song",
     artist: "Type R",
-    audio_url: "undernet-vol-4/Type-R-Reas_Song",
+    audio_url: "undernet-vol-4/Type_R-Reas_Song",
     permalink_url: "https://soundcloud.com/underdotnet/type-r-rea_s-song-4",
   },
   {
@@ -43,6 +43,84 @@ export const underNet: Tracklist = [
     audio_url: "undernet-vol-4/luuxu-wisp",
     permalink_url: "https://soundcloud.com/underdotnet/luuxu-wisp-7",
   },
+  {
+    title: "JOINMYPARTY",
+    artist: "C3NTELL4",
+    audio_url: "undernet-vol-4/C3NTELL4-JOINMYPARTY",
+    permalink_url: "https://soundcloud.com/underdotnet/c3ntell4-joinmyparty-8",
+  },
+  {
+    title: "iNSERTname",
+    artist: "zhnoi",
+    audio_url: "undernet-vol-4/zhnoi-iNSERTname",
+    permalink_url: "https://soundcloud.com/underdotnet/zhnoi-insertname-9",
+  },
+  {
+    title: "lifelover",
+    artist: "dreamloader",
+    audio_url: "undernet-vol-4/dreamloader-lifelover",
+    permalink_url: "https://soundcloud.com/underdotnet/dreamloader-lifelover-10"
+  },
+  {
+    title: "Dematerialize N2",
+    artist: "Synami",
+    audio_url: "undernet-vol-4/Synami-Dematerialize_N2",
+    permalink_url: "https://soundcloud.com/underdotnet/synami-dematerialize-n2-11"
+  },
+  {
+    title: "LOST SKY",
+    artist: "EDENA",
+    audio_url: "undernet-vol-4/EDENA-LOST_SKY",
+    permalink_url: "https://soundcloud.com/underdotnet/edena-lost-sky-12"
+  },
+  {
+    title: "beyond ur eyes",
+    artist: "servallize",
+    audio_url: "undernet-vol-4/servallize-beyond_ur_eyes",
+    permalink_url: "https://soundcloud.com/underdotnet/servallize-beyond-ur-eyes-13"
+  },
+  {
+    title: "MUSE",
+    artist: "sixteen_veins",
+    audio_url: "undernet-vol-4/sixteen_veins-MUSE",
+    permalink_url: "https://soundcloud.com/underdotnet/sixteen-veins-muse-14"
+  },
+  {
+    title: "FEAR",
+    artist: "Ghost_In_The_System",
+    audio_url: "undernet-vol-4/Ghost_In_The_System-FEAR",
+    permalink_url: "https://soundcloud.com/underdotnet/ghost-in-the-system-fear-15"
+  },
+  {
+    title: "ANG3L_CRUSH",
+    artist: "earthangelbaby",
+    audio_url: "undernet-vol-4/earthangelbaby-ANG3L_CRUSH",
+    permalink_url: "https://soundcloud.com/underdotnet/earthangelbaby-ang3l-crush-16"
+  },
+  {
+    title: "HATENA",
+    artist: "GnB_Chili",
+    audio_url: "undernet-vol-4/GnB_Chili-HATENA",
+    permalink_url: "https://soundcloud.com/underdotnet/gnb-chili-hatena-17"
+  },
+  {
+    title: "WAKE_ME_UP",
+    artist: "nabii",
+    audio_url: "undernet-vol-4/nabii-WAKE_ME_UP",
+    permalink_url: "https://soundcloud.com/underdotnet/nabii-wake-me-up-18"
+  },
+  {
+    title: "Its_Better_to_Burn_Out_Than_Fade_Away",
+    artist: "enable_secret",
+    audio_url: "undernet-vol-4/enable_secret-Its_Better_to_Burn_Out_Than_Fade_Away",
+    permalink_url: "https://soundcloud.com/underdotnet/enable-secret-its-better-to-burn-out-than-fade-away-19"
+  },
+  {
+    title: "ultravision",
+    artist: "DWAMS",
+    audio_url: "undernet-vol-4/DWAMS-ultravision",
+    permalink_url: "https://soundcloud.com/underdotnet/dwams-ultravision-20"
+  }
 ];
 
 export const playlists: Playlist[] = [
